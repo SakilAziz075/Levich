7e33742f5f39f21efd3250d5b58f2b6bb9989dea
Sakil-Aziz <sakilaziz12@gmail.com>
2026-01-29 13:36:17 +0530

    import port related changes for render

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,23 @@
-// HTTP Server + Socket
-
 const http = require("http");
 const app = require("./app");
 
-const { PORT } = require("./config");
 const logger = require("./common/logger");
-
 const initSocket = require("./socket");
 const seedItems = require("./seed/seedItems");
 
+// Safety nets
+process.on("unhandledRejection", err => {
+  console.error("Unhandled Rejection:", err);
+});
+
+process.on("uncaughtException", err => {
+  console.error("Uncaught Exception:", err);
+});
+
 const server = http.createServer(app);
+
+// IMPORTANT FOR RENDER
+const PORT = process.env.PORT || 4000;
 
 seedItems();
 initSocket(server);
